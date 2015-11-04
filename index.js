fafddbc8d11c8e44270a26ba7047b6a11296d687
lesliepajuelo <l.l.pajuelo@gmail.com>
2015-11-04 15:22:58 -0800

    Prepare for Heroku push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('./server.js');
 
  var port = 4568;
-
-app.listen(port);
+//Set server to listen on an environment-specific port or default to provided port;
+app.listen(process.env.PORT || port);
 
 console.log('Server now listening on port ' + port);
